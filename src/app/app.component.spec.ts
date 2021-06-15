b23f75736218f8c2cd36a1240423e79b555d2dc3
Gauri Pathak <gauri.pathak@global.internal>
2021-06-15 13:35:11 +0530

    Added Reactive Form Imports

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,8 @@
 import { async, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatList } from '@angular/material/list';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -6,8 +10,10 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
-      ],
+        RouterTestingModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        FormsModule, ],
       declarations: [
         AppComponent
       ],
@@ -29,7 +35,7 @@ describe('AppComponent', () => {
   it(`the title is 'my-awesome-app'`, (() => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
-    expect(app.title).toEqual('my-awesome-app')
+    expect(app.title).toEqual('my-awesome-app');
   }))
 
   // describe('AppComponent',()=>{
